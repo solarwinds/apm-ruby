f75ef813183921b12d9f1feb87b094829858ff75
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-07-18 14:08:13 -0400

    NH-46935: add ubuntu and amzlinux

--- .github/workflows/scripts/verify_install.sh
+++ .github/workflows/scripts/verify_install.sh
@@ -1,11 +1,39 @@
 # Copyright (c) SolarWinds, LLC.
 # All rights reserved.
 
+
 # setup the system
+pretty_name=$(grep PRETTY_NAME /etc/os-release | sed 's/PRETTY_NAME="//' | sed 's/"//')
 if [ -r /etc/alpine-release ]; then
   apk update && apk add --upgrade ruby-dev g++ make
 elif [ -r /etc/debian_version ]; then
+  # this is for ubuntu (> 22.04) and debian
   apt-get update && apt-get install -y ruby-dev g++ make
+elif [ $pretty_name = "Amazon Linux 2" ]; then
+  amazon-linux-extras install epel -y
+  yum update && yum install -y ruby-devel gcc-c++ make tar openssl-devel git
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
+    && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build \
+    && git clone https://github.com/rbenv/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems \
+    && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile \
+    && echo 'eval "$(rbenv init -)"' >> ~/.profile \
+    && echo 'eval "$(rbenv init -)"' >> ~/.bashrc \
+    && echo 'bundler' > ~/.rbenv/default-gems
+  . ~/.profile && rbenv install 3.1.0
+  rbenv local 3.1.0
+
+elif [ $pretty_name = "Amazon Linux 2023" ]; then
+
+  yum update && yum install -y ruby-devel gcc-c++ make tar openssl-devel git zlib-devel
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
+    && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build \
+    && git clone https://github.com/rbenv/rbenv-default-gems.git ~/.rbenv/plugins/rbenv-default-gems \
+    && echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile \
+    && echo 'eval "$(rbenv init -)"' >> ~/.profile \
+    && echo 'eval "$(rbenv init -)"' >> ~/.bashrc \
+    && echo 'bundler' > ~/.rbenv/default-gems
+  . ~/.profile && rbenv install 3.1.0
+  rbenv local 3.1.0
 fi
 
 # install gem
