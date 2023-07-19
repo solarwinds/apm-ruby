0d922fba1efc35bbc8f596934ab5d05831d09b92
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-07-19 10:50:13 -0400

    NH-46935: update workflow

--- .github/workflows/scripts/verify_install.sh
+++ .github/workflows/scripts/verify_install.sh
@@ -1,7 +1,6 @@
 # Copyright (c) SolarWinds, LLC.
 # All rights reserved.
 
-
 # setup the system
 pretty_name=$(grep PRETTY_NAME /etc/os-release | sed 's/PRETTY_NAME="//' | sed 's/"//')
 if [ -r /etc/alpine-release ]; then
@@ -9,7 +8,7 @@ if [ -r /etc/alpine-release ]; then
 elif [ -r /etc/debian_version ]; then
   # this is for ubuntu (> 22.04) and debian
   apt-get update && apt-get install -y ruby-dev g++ make
-elif [ $pretty_name = "Amazon Linux 2" ]; then
+elif [ "$pretty_name" = "Amazon Linux 2" ]; then
   amazon-linux-extras install epel -y
   yum update && yum install -y ruby-devel gcc-c++ make tar openssl-devel git
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
@@ -21,9 +20,7 @@ elif [ $pretty_name = "Amazon Linux 2" ]; then
     && echo 'bundler' > ~/.rbenv/default-gems
   . ~/.profile && rbenv install 3.1.0
   rbenv local 3.1.0
-
-elif [ $pretty_name = "Amazon Linux 2023" ]; then
-
+elif [ "$pretty_name" = "Amazon Linux 2023" ]; then
   yum update && yum install -y ruby-devel gcc-c++ make tar openssl-devel git zlib-devel
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv \
     && git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build \
