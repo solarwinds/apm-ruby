f4a1473f398dff27a25187c4c6f5fbc4963728dd
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-08-09 17:23:54 -0400

    NH-42262: add valid trace test

--- test/unit/trace_context_log_test.rb
+++ test/unit/trace_context_log_test.rb
@@ -75,4 +75,86 @@ describe 'Trace Context in Log Test' do
     @log_output.rewind
     assert_includes @log_output.read, 'trace_id=00000000000000000000000000000000 span_id=0000000000000000 trace_flags=00 resource.service.name='
   end
+
+  it 'test_log_traceId_with_debug_always_valid_span' do
+    SolarWindsAPM.logger.level = Logger::DEBUG
+    SolarWindsAPM::Config[:log_traceId] = :sampled
+
+    OpenTelemetry::SDK.configure do |c|
+      c.service_name = 'my_service'
+      c.add_span_processor(::OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(::OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new))
+    end
+
+    OpenTelemetry.tracer_provider.tracer('my_service').in_span('sample_span') do |span|
+      span.context.trace_flags.instance_variable_set(:@flags, 1)
+      SolarWindsAPM.logger.debug "Sample debug message"
+    end
+    
+    @log_output.rewind
+    log_output = @log_output.read
+
+    trace_id = log_output.match(/trace_id=([\da-fA-F]+)/)
+
+    assert_equal(trace_id&.size, 2)
+    assert_equal(trace_id[1]&.size, 32)
+    
+    span_id  = log_output.match(/span_id=([\da-fA-F]+)/)
+    assert_equal(span_id&.size, 2)
+    assert_equal(span_id[1]&.size, 16)
+
+    trace_flags  = log_output.match(/trace_flags=([\da-fA-F]+)/)
+    assert_equal(trace_flags&.size, 2)
+    assert_equal(trace_flags[1]&.size, 2)
+  end
+
+  it 'test_log_traceId_with_debug_never_valid_span' do
+
+    SolarWindsAPM.logger.level = Logger::DEBUG
+    SolarWindsAPM::Config[:log_traceId] = :never
+
+    OpenTelemetry::SDK.configure do |c|
+      c.service_name = 'my_service'
+      c.add_span_processor(::OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(::OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new))
+    end
+
+    OpenTelemetry.tracer_provider.tracer('my_service').in_span('sample_span') do |_span|
+      SolarWindsAPM.logger.debug "Sample debug message"
+    end
+    
+    @log_output.rewind
+    log_output = @log_output.read
+    trace_id = log_output.match(/trace_id=([\da-fA-F]+)/)
+    span_id  = log_output.match(/span_id=([\da-fA-F]+)/)
+    trace_flags  = log_output.match(/trace_flags=([\da-fA-F]+)/)
+
+    assert_nil(trace_id) 
+    assert_nil(span_id)
+    assert_nil(trace_flags)
+  end
+
+  it 'test_log_traceId_with_debug_never_valid_span_untraced' do
+
+    SolarWindsAPM.logger.level = Logger::DEBUG
+    SolarWindsAPM::Config[:log_traceId] = :sampled
+
+    OpenTelemetry::SDK.configure do |c|
+      c.service_name = 'my_service'
+      c.add_span_processor(::OpenTelemetry::SDK::Trace::Export::SimpleSpanProcessor.new(::OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new))
+    end
+
+    OpenTelemetry.tracer_provider.tracer('my_service').in_span('sample_span') do |span|
+      span.context.trace_flags.instance_variable_set(:@flags, 0) # force span to untraced so sampled won't append value
+      SolarWindsAPM.logger.debug "Sample debug message"
+    end
+    
+    @log_output.rewind
+    log_output = @log_output.read
+    trace_id = log_output.match(/trace_id=([\da-fA-F]+)/)
+    span_id  = log_output.match(/span_id=([\da-fA-F]+)/)
+    trace_flags  = log_output.match(/trace_flags=([\da-fA-F]+)/)
+
+    assert_nil(trace_id) 
+    assert_nil(span_id)
+    assert_nil(trace_flags)
+  end
 end
