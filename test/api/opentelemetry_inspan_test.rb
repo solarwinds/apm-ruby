643347443e89e442081a80810d0edb641d325e38
xuan-cao-swi <xuan.cao@solarwinds.com>
2023-10-25 13:03:23 -0400

    NH-65512: fix

--- test/api/opentelemetry_inspan_test.rb
+++ test/api/opentelemetry_inspan_test.rb
@@ -3,15 +3,17 @@
 
 require 'minitest_helper'
 require './lib/solarwinds_apm/api'
+# act -j unit_test --container-architecture linux/arm64 -s SW_APM_SERVICE_KEY=your_key
+# 
 
 describe 'SolarWinds Set Transaction Name Test' do
   before do
-    @op = -> { 1.times {[9, 6, 12, 2, 7, 1, 9, 3, 4, 14, 5, 8].sort} }
+    @op = -> { 10.times {[9, 6, 12, 2, 7, 1, 9, 3, 4, 14, 5, 8].sort} }
   end
 
   it 'test_in_span_wrapper_from_solarwinds_apm' do
 
-    in_memory_exporter = CustomInMemorySpanExporter.new
+    in_memory_exporter = ::OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new
     OpenTelemetry::SDK.configure do |c|
       c.service_name = 'test_in_span_wrapper_from_solarwinds_apm'
       c.add_span_processor(
@@ -23,7 +25,7 @@ describe 'SolarWinds Set Transaction Name Test' do
       @op.call
     end
 
-    finished_spans = in_memory_exporter.finished_spans
+    finished_spans = extract_span(in_memory_exporter)
 
     _(finished_spans.first.name).must_equal 'custom_span'
 
@@ -32,7 +34,7 @@ describe 'SolarWinds Set Transaction Name Test' do
 
   it 'test_in_span_wrapper_from_solarwinds_apm_with_span' do
     
-    in_memory_exporter = CustomInMemorySpanExporter.new
+    in_memory_exporter = ::OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new
     OpenTelemetry::SDK.configure do |c|
       c.service_name = 'test_in_span_wrapper_from_solarwinds_apm_with_span'
       c.add_span_processor(
@@ -45,7 +47,7 @@ describe 'SolarWinds Set Transaction Name Test' do
       @op.call
     end
 
-    finished_spans = in_memory_exporter.finished_spans
+    finished_spans = extract_span(in_memory_exporter)
 
     _(finished_spans.first.name).must_equal 'custom_span'
     _(finished_spans.first.attributes['test_attribute']).must_equal 'attribute_1'
@@ -55,7 +57,7 @@ describe 'SolarWinds Set Transaction Name Test' do
 
   it 'test_in_span_wrapper_from_solarwinds_apm_without_block' do
 
-    in_memory_exporter = CustomInMemorySpanExporter.new
+    in_memory_exporter = ::OpenTelemetry::SDK::Trace::Export::InMemorySpanExporter.new
     OpenTelemetry::SDK.configure do |c|
       c.service_name = 'test_in_span_wrapper_from_solarwinds_apm'
       c.add_span_processor(
@@ -65,7 +67,7 @@ describe 'SolarWinds Set Transaction Name Test' do
 
     SolarWindsAPM::API.in_span('custom_span')
 
-    finished_spans = in_memory_exporter.finished_spans
+    finished_spans = extract_span(in_memory_exporter)
     _(finished_spans.size).must_equal 0
 
     in_memory_exporter.shutdown
