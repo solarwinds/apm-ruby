6a7880ad0d2efe4f8817e9fc55dfae5094610c6e
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-01-16 09:59:08 -0500

    NH-38602: start the reporter despite of worker or master

--- lib/solarwinds_apm/base.rb
+++ lib/solarwinds_apm/base.rb
@@ -11,18 +11,6 @@ module SolarWindsAPMBase
   extend SolarWindsAPM::ThreadLocal
 
   attr_accessor :reporter, :loaded
-
-  ##
-  # Determines if we are running under a forking webserver
-  #
-  def forking_webserver?
-    if (defined?(::Unicorn) && ($PROGRAM_NAME =~ /unicorn/i)) ||
-       (defined?(::Puma) && ($PROGRAM_NAME =~ /puma/i))
-      true
-    else
-      false
-    end
-  end
 end
 
 module SolarWindsAPM
