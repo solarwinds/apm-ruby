6c6503f4e67256993b70701681b05663bcfd9d58
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-01-26 11:25:20 -0500

    NH-70338: version-update

--- lib/solarwinds_apm/version.rb
+++ lib/solarwinds_apm/version.rb
@@ -12,7 +12,7 @@ module SolarWindsAPM
     MAJOR  = 6 # breaking,
     MINOR  = 0 # feature,
     PATCH  = 0 # fix => BFF
-    PRE    = 'prev5'.freeze
+    PRE    = 'prev6'.freeze
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
   end
