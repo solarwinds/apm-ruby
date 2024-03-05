88b04d83fa20f81b84b1efa9158bc2466bc02aeb
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-03-04 20:33:07 -0500

    NH-69805: lint

--- lambda/layer/wrapper.rb
+++ lambda/layer/wrapper.rb
@@ -5,6 +5,6 @@ require 'opentelemetry/instrumentation/aws_lambda'
 ::OpenTelemetry::Instrumentation.registry.install(['OpenTelemetry::Instrumentation::AwsLambda'])
 
 def otel_wrapper(event:, context:)
-  otel_wrapper = OpenTelemetry::Instrumentation::AwsLambda::Handler.new()
+  otel_wrapper = OpenTelemetry::Instrumentation::AwsLambda::Handler.new
   otel_wrapper.call_wrapped(event: event, context: context)
 end
