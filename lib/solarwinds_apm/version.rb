a9f1c786127e5e70208209077297648e5f87cc1a
xuan-cao-swi <xuan.cao@solarwinds.com>
2024-06-19 12:35:23 -0400

    NH-82147: update oboe version and ready for release

--- lib/solarwinds_apm/version.rb
+++ lib/solarwinds_apm/version.rb
@@ -12,7 +12,7 @@ module SolarWindsAPM
   # solarwinds_apm.gemspec during gem build process
   module Version
     MAJOR  = 6 # breaking,
-    MINOR  = 0 # feature,
+    MINOR  = 1 # feature,
     PATCH  = 0 # fix => BFF
     PRE    = nil
 
