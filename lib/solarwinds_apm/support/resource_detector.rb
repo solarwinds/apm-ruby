648cf0a169b731a3da3c5420c347bbe92c5283f8
xuan-cao-swi <xuan.cao@solarwinds.com>
2025-03-21 12:18:44 -0400

    NH-103815: add in-house resource detector

---
+++ lib/solarwinds_apm/support/resource_detector.rb
@@ -0,0 +1,133 @@
+# frozen_string_literal: true
+
+# © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at:http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+module SolarWindsAPM
+  # ResourceDetector
+  # Usage:
+  #   require 'opentelemetry/sdk'
+  #   require 'opentelemetry/resource/detector'
+  #   OpenTelemetry::SDK.configure do |c|
+  #     c.resource = SolarWindsAPM::ResourceDetector.detect
+  #   end
+  module ResourceDetector
+    K8S_PODNAME_PATH = '/etc/hostname'
+    K8S_NAMESPACE_PATH = '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
+    K8S_TOKEN_PATH = '/var/run/secrets/kubernetes.io/serviceaccount/token'
+
+    UAMS_CLIENT_PATH = '/opt/solarwinds/uamsclient/var/uamsclientid'
+    UAMS_CLIENT_URL = 'http://127.0.0.1:2113/info/uamsclient'
+    UAMS_CLIENT_ID_FIELD = 'uamsclient_id'
+
+    def self.detect
+      attributes = ::OpenTelemetry::SDK::Resources::Resource.create({})
+      attributes = attributes.merge(detect_uams_client_id)
+      attributes = attributes.merge(detect_k8s_atttributes)
+      attributes = attributes.merge(from_upstream_detector)
+      attributes
+    end
+
+    def self.detect_uams_client_id
+      uams_client_id = nil
+      if File.exist?(UAMS_CLIENT_PATH)
+        uams_client_id = File.read(UAMS_CLIENT_PATH).strip
+      else
+        url = URI(UAMS_CLIENT_URL)
+
+        response = nil
+        ::OpenTelemetry::Common::Utilities.untraced do
+          http = Net::HTTP.new(url.host, url.port)
+          request = Net::HTTP::Get.new(url)
+          response = http.request(request)
+        end
+
+        if response&.code.to_i == 200
+          uams_metadata = JSON.parse(response.body)
+          uams_client_id = uams_metadata&.fetch(UAMS_CLIENT_ID_FIELD)
+        end
+      end
+
+      resource_attributes = {
+        'sw.uams.client.id' => uams_client_id
+      }
+
+      ::OpenTelemetry::SDK::Resources::Resource.create(resource_attributes)
+    rescue StandardError => e
+      SolarWindsAPM.logger.debug "#{self.class}/#{__method__}] detect_uams_client_id failed. Error: #{e.message}."
+      ::OpenTelemetry::SDK::Resources::Resource.create({})
+    end
+
+    def self.detect_k8s_atttributes
+
+      return ::OpenTelemetry::SDK::Resources::Resource.create({}) unless ENV['KUBERNETES_SERVICE_HOST'] && ENV['KUBERNETES_SERVICE_PORT']
+
+      resource_attributes = {}
+      pod_name = File.read(K8S_PODNAME_PATH).strip if File.exist?(K8S_PODNAME_PATH)
+      namespace = File.read(K8S_NAMESPACE_PATH).strip if File.exist?(K8S_NAMESPACE_PATH)
+      pod_uid = nil
+      token = nil
+
+      token = File.read(K8S_TOKEN_PATH).strip if File.exist?(K8S_TOKEN_PATH)
+
+      if token && pod_name && namespace
+        url = URI("https://kubernetes.default.svc/api/v1/namespaces/#{namespace}/pods/#{pod_name}")
+
+        response = nil
+        ::OpenTelemetry::Common::Utilities.untraced do
+          http = Net::HTTP.new(url.host, url.port)
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+          request = Net::HTTP::Get.new(url)
+          request['Authorization'] = "Bearer #{token}"
+          request['Accept'] = 'application/json'
+
+          response = http.request(request)
+        end
+
+        if response&.code.to_i == 200
+          pod_metadata = JSON.parse(response.body)
+          pod_uid = pod_metadata&.fetch('metadata')&.fetch('uid')
+        end
+      end
+
+      resource_attributes['k8s.namespace.name'] = namespace
+      resource_attributes['k8s.pod.name'] = pod_name
+      resource_attributes['k8s.pod.uid'] = pod_uid
+      
+      resource_attributes.compact!
+      ::OpenTelemetry::SDK::Resources::Resource.create(resource_attributes)
+    end
+
+    def self.from_upstream_detector
+      require_detector('opentelemetry-resource-detector-google_cloud_platform')
+      require_detector('opentelemetry-resource-detector-container')
+      require_detector('opentelemetry-resource-detector-azure')
+
+      resource_attributes = ::OpenTelemetry::SDK::Resources::Resource.create({})
+      resource_attributes = resource_attributes.merge(::OpenTelemetry::Resource::Detector::Azure.detect) if defined? OpenTelemetry::Resource::Detector::Azure
+      resource_attributes = resource_attributes.merge(::OpenTelemetry::Resource::Detector::GoogleCloudPlatform.detect) if defined? OpenTelemetry::Resource::Detector::GoogleCloudPlatform
+      resource_attributes = resource_attributes.merge(::OpenTelemetry::Resource::Detector::Container.detect) if defined? OpenTelemetry::Resource::Detector::Container
+
+      SolarWindsAPM.logger.debug { "#{self.class}/#{__method__}] resource_attributes: #{resource_attributes.inspect}" }
+
+      resource_attributes
+    end
+
+    def self.require_detector(gem_name)
+      begin
+        require gem_name
+      rescue StandardError => e
+        SolarWindsAPM.logger.warn { "No #{gem_name} found." }
+      end
+    end
+  end
+end
---
+++ test/support/resource_detector_test.rb
@@ -0,0 +1,113 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2023 SolarWinds, LLC.
+# All rights reserved.
+
+require 'minitest_helper'
+require 'webmock'
+require './lib/solarwinds_apm/support/resource_detector'
+
+describe 'Resource Detector Test' do
+  it 'detect_k8s_atttributes_with_valid_path' do
+    ENV['KUBERNETES_SERVICE_HOST'] = '10.96.0.1'
+    ENV['KUBERNETES_SERVICE_PORT'] = '443'
+
+    FileUtils.mkdir_p('/var/run/secrets/kubernetes.io/serviceaccount/')
+    File.open(SolarWindsAPM::ResourceDetector::K8S_NAMESPACE_PATH, 'w') do |file|
+      file.puts("fake_namespace")
+    end
+    File.open(SolarWindsAPM::ResourceDetector::K8S_TOKEN_PATH, 'w') do |file|
+      file.puts("fake@token")
+    end
+
+    if File.exist?(SolarWindsAPM::ResourceDetector::K8S_PODNAME_PATH)
+      original_hostname = File.read(SolarWindsAPM::ResourceDetector::K8S_PODNAME_PATH).strip!
+    else
+      original_hostname = nil
+    end
+
+    File.open(SolarWindsAPM::ResourceDetector::K8S_PODNAME_PATH, 'w') do |file|
+      file.puts("fake_hostname")
+    end
+
+    WebMock.disable_net_connect!
+    WebMock.enable!
+    WebMock.stub_request(:get, "https://kubernetes.default.svc/api/v1/namespaces/fake_namespace/pods/fake_hostname")
+           .to_return(
+             status: 200,
+             body: {"kind"=>"Pod", "metadata"=>{"uid"=>"fake_uid"}}.to_json,
+             headers: { 'Content-Type' => 'application/json' }
+           )
+    attributes = SolarWindsAPM::ResourceDetector.detect_k8s_atttributes
+
+    _(attributes.instance_variable_get(:@attributes)['k8s.namespace.name']).must_equal 'fake_namespace'
+    _(attributes.instance_variable_get(:@attributes)['k8s.pod.name']).must_equal 'fake_hostname'
+    _(attributes.instance_variable_get(:@attributes)['k8s.pod.uid']).must_equal 'fake_uid'
+
+    File.delete(SolarWindsAPM::ResourceDetector::K8S_NAMESPACE_PATH)
+    File.delete(SolarWindsAPM::ResourceDetector::K8S_TOKEN_PATH)
+    if original_hostname.nil?
+      File.delete(SolarWindsAPM::ResourceDetector::K8S_PODNAME_PATH)
+    else
+      File.open(SolarWindsAPM::ResourceDetector::K8S_PODNAME_PATH, 'w') do |file|
+        file.puts(original_hostname)
+      end
+    end
+
+    ENV.delete('KUBERNETES_SERVICE_HOST')
+    ENV.delete('KUBERNETES_SERVICE_PORT')
+    WebMock.reset!
+    WebMock.allow_net_connect!
+  end
+
+  it 'detect_k8s_atttributes_with_invalid_path' do
+    ENV['KUBERNETES_SERVICE_HOST'] = '10.96.0.1'
+    ENV['KUBERNETES_SERVICE_PORT'] = '443'
+
+    attributes = SolarWindsAPM::ResourceDetector.detect_k8s_atttributes
+    assert_nil(attributes.instance_variable_get(:@attributes)['k8s.pod.uid'])
+    assert_nil(attributes.instance_variable_get(:@attributes)['k8s.namespace.name'])
+
+    ENV.delete('KUBERNETES_SERVICE_HOST')
+    ENV.delete('KUBERNETES_SERVICE_PORT')
+  end
+
+  it 'return_empty_if_not_in_k8s' do
+    attributes = SolarWindsAPM::ResourceDetector.detect_k8s_atttributes
+    assert_equal(attributes.instance_variable_get(:@attributes), {})
+  end
+
+  it 'detect_uams_client_id_failed' do
+    attributes = SolarWindsAPM::ResourceDetector.detect_uams_client_id
+    assert_nil(attributes.instance_variable_get(:@attributes)['sw.uams.client.id'])
+  end
+
+  it 'detect_uams_client_id_from_file' do
+    FileUtils.mkdir_p('/opt/solarwinds/uamsclient/var')
+    File.open(SolarWindsAPM::ResourceDetector::UAMS_CLIENT_PATH, 'w') do |file|
+      file.puts("fake_uams_client_id")
+    end
+
+    attributes = SolarWindsAPM::ResourceDetector.detect_uams_client_id
+
+    _(attributes.instance_variable_get(:@attributes)['sw.uams.client.id']).must_equal 'fake_uams_client_id'
+    File.delete(SolarWindsAPM::ResourceDetector::UAMS_CLIENT_PATH)
+  end
+
+  it 'detect_uams_client_id_from_local_url' do
+    WebMock.disable_net_connect!
+    WebMock.enable!
+    WebMock.stub_request(:get, SolarWindsAPM::ResourceDetector::UAMS_CLIENT_URL)
+           .to_return(
+             status: 200,
+             body: { SolarWindsAPM::ResourceDetector::UAMS_CLIENT_ID_FIELD => "12345", "status" => "active" }.to_json,
+             headers: { 'Content-Type' => 'application/json' }
+           )
+
+    attributes = SolarWindsAPM::ResourceDetector.detect_uams_client_id
+    _(attributes.instance_variable_get(:@attributes)['sw.uams.client.id']).must_equal '12345'
+    WebMock.reset!
+    WebMock.allow_net_connect!
+  end
+
+end
