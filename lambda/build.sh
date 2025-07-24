7b28f257744381af377e53c4aaf2ff151e1d3f0c
xuan-cao-swi <xuan.cao@solarwinds.com>
2025-07-24 11:47:23 -0400

    improve layer build

--- lambda/build.sh
+++ lambda/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+echo "Build from source: $SOLARWINDS_SOURCE. Publish to $PUBLISH_DEST."
 if [ $SOLARWINDS_SOURCE = 'Local' ]; then
     cd ../
     sudo apt-get update && sudo apt-get install -y --no-install-recommends ruby ruby-dev g++ make
@@ -16,6 +17,7 @@ fi
 mkdir -p build
 
 for ruby_version in 3.2 3.3 3.4; do
+  echo "Build container for ruby $ruby_version."
   docker build --no-cache \
                --build-arg RUBY_VERSION=${ruby_version} \
                --progress plain \
@@ -29,4 +31,5 @@ cd build/
 mkdir solarwinds-apm && mkdir ruby && mkdir ruby/gems
 unzip -q gems-3.2.0.zip -d ruby/gems/ && unzip -q gems-3.3.0.zip -d ruby/gems/ && unzip -q gems-3.4.0.zip -d ruby/gems/
 cp ../otel/layer/otel_wrapper.rb . && cp ../otel/layer/wrapper solarwinds-apm/
+echo "Zip built solarwinds_apm and installed opentelemetry gem into single layer."
 zip -qr ruby-layer.zip ruby/ solarwinds-apm/ otel_wrapper.rb
